926fc3f63896d4f5a7a39f05beb87691f0967f81
Lourenço Viana <lourenco.m.c.viana@gmail.com>
2018-08-22 15:52:42 -0300

    simplificação do script

--- script/role.sql
+++ script/role.sql
@@ -1,26 +1,25 @@
-CREATE ROLE logger;
+-- Interface para ser usada por aplicações
+grant execute on log_audit.PKG_log_i to youruser;
 
-grant execute on log_audit.PKG_log to logger;
-grant execute on log_audit.PKG_log_i to logger;
-grant execute on log_audit.PKG_util to logger;
-grant select on log_audit.v_log to logger;
-grant select on log_audit.v_log_recent to logger;
-grant select on log_audit.v_parameter to logger;
-grant select on log_audit.v_Context to logger;
-grant select on log_audit.v_priority to logger;
-grant select on log_audit.v_update to logger;
+-- Permissões para utilziar o pkg_log dentro da base de dados
+grant execute on log_audit.PKG_log to youruser;
+grant execute on log_audit.PKG_log_i to youruser;
+grant execute on log_audit.PKG_util to youruser;
+grant select on log_audit.v_log to youruser;
+grant select on log_audit.v_log_recent to youruser;
+grant select on log_audit.v_parameter to youruser;
+grant select on log_audit.v_Context to youruser;
+grant select on log_audit.v_priority to youruser;
+grant select on log_audit.v_update to youruser;
 
-grant select on log_audit.t_priority to logger;
-grant select on log_audit.t_log to logger;
-grant select on log_audit.t_update to logger;
-grant select on log_audit.t_context to logger;
-grant select on log_audit.t_context_node to logger;
-grant select on log_audit.t_parameter to logger;
-grant select on log_audit.t_parameter_clob to logger;
-grant select on log_audit.t_parameter_date to logger;
-grant select on log_audit.t_parameter_xml to logger;
-grant select on log_audit.t_parameter_number to logger;
+grant select on log_audit.t_priority to youruser;
+grant select on log_audit.t_log to youruser;
+grant select on log_audit.t_update to youruser;
+grant select on log_audit.t_context to youruser;
+grant select on log_audit.t_context_node to youruser;
+grant select on log_audit.t_parameter to youruser;
+grant select on log_audit.t_parameter_clob to youruser;
+grant select on log_audit.t_parameter_date to youruser;
+grant select on log_audit.t_parameter_xml to youruser;
+grant select on log_audit.t_parameter_number to youruser;
 
-CREATE ROLE logger_i;
-
-grant execute on log_audit.PKG_log_i to logger_i;
